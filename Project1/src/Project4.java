050a4074dc07a858994876c82a80f121ac75ead6
naskapal <co.terbang@gmail.com>
2012-11-14 18:17:49 +0800

    14-11-2012 6:17PM

--- Project1/src/Project4.java
+++ Project1/src/Project4.java
@@ -2,27 +2,23 @@ import java.util.*;
 public class Project4 {
 
 	public static void main (String[] args){
-		double i = 1, totalAmount = 0, weight = 0;
+		double i = 1, totalAmount = 0, weight = 0, cost = 0;
 		char dest = 'a';
 		int numOfIParcels = 0, numOfDParcels = 0, rejectedParcels = 0;
-		method myclass = new method();
-		
 		method.introductionMessage();
 		while (i != 0)
 		{
-			Scanner sc1 = new Scanner(System.in);
-			myclass.getParcelWeight();
-			weight = sc1.nextDouble();
+			weight = method.getParcelWeight();
 			i = weight;
-			myclass.validParcelDestination(dest, weight);
-		
+			if (i > 0)
+			{
 		Scanner sc2 = new Scanner(System.in);
-		myclass.getParcelDestination();
+		method.getParcelDestination();
 		dest = sc2.next().charAt(0);
-		myclass.validParcelDestination(dest, weight);
+		method.validParcelDestination(dest, weight);
 		if (dest == 'D' || dest == 'd')
 		{
-			myclass.computeDomesticCost(weight);
+			cost = method.computeDomesticCost(weight);
 			numOfDParcels++;
 			if (weight <= 0)
 			{
@@ -31,14 +27,15 @@ public class Project4 {
 		}
 		else if (dest == 'I' || dest == 'i')
 		{
-			myclass.computeInternationalCost(weight);
+			cost = method.computeInternationalCost(weight);
 			numOfIParcels++;
 			if (weight <= 0)
 			{
 				numOfIParcels--;
 			}
+		}totalAmount = totalAmount + cost;
 		}
-		}
-		myclass.displayFinalOutput(numOfIParcels, numOfDParcels, rejectedParcels, totalAmount);
+		method.displayFinalOutput(numOfIParcels, numOfDParcels, rejectedParcels, totalAmount);
 	}
+}
 }
--- Project1/src/method.java
+++ Project1/src/method.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 public class method {
-	public static int numOfDParcels = 0, numOfIParcels = 0, rejectedParcels = 0;
-	public static double weight = 0, totalAmount = 0, grams = 0;
+	public static int numOfDParcels = 0, numOfIParcels = 0, rejected = 0, i = 1;
+	public static double weight, totalAmount = 0, grams = 0, cost = 0;
+	public static boolean check;
+	static Scanner sc1 = new Scanner(System.in);
 public static void introductionMessage () {
 	System.out.println("Welcome");
 	System.out.println("With this program, you can calculate the total cost of posting numbers of parcels");
 }
 public static double getParcelWeight () {
-	double weight = 0;
-	System.out.print("Enter the parcel weight in kg :");
+	System.out.print("Enter the parcel weight in kg : ");
+	weight = sc1.nextDouble();
+	while (weight < 0)
+	{
+		System.out.print("You must enter non-negative number");
+		System.out.print("Enter the parcel weight in kg : ");
+		weight = sc1.nextDouble();
+	}
 	return weight;
 }
 public static char getParcelDestination () {
@@ -18,72 +27,74 @@ public static char getParcelDestination () {
 public static boolean validParcelDestination (char dest, double weight) {
 	if (dest != 'D' && dest != 'd' && dest != 'I' && dest != 'i')
 	{
-		System.out.println("Please enter 'D', 'd', 'I', or 'i' only");
+		check = true;
 	}
-	if (dest == 'I' || dest == 'i' && weight > 20)
+	if (weight > 20 && dest == 'I' || dest == 'i')
 	{
-		System.out.println("Sorry, you can't send parcel weighing more than 20.00 kg oversea");
+		check = true;
 	}
-	else if (dest == 'D' || dest == 'd' && weight > 30)
+	else if (weight > 30 && dest == 'D' || dest == 'd')
 	{
-		 System.out.println("Sorry, you can't send parcel weighing more than 30.00 kg within Malaysia");
+		 check = true;
 	}
-	else if (weight < 0)
-	{
-		System.out.println("You must enter non-negative number");
-		System.out.println("Enter the parcel weight in kg ");
-	}return false;
+	return check;
 }
 public static double computeDomesticCost (double weight) {
 	if (weight > 0 && weight <1)
-	{totalAmount = 5.50;
+	{cost = 5.50;
    		System.out.print("For a parcel weight of " + weight);
-   	 	System.out.println("kg, the cost is RM" + totalAmount);
+   	 	System.out.println("kg, the cost is RM" + cost);
 	}
 	else if (weight >=1 && weight<5) 
    	{
-   		totalAmount = weight * 6.50;
+   		cost = weight * 6.50;
    		System.out.print("For a parcel weight of " + weight);
-   	 	System.out.println("kg, the cost is RM" + totalAmount);
+   	 	System.out.println("kg, the cost is RM" + cost);
    	}
    	else if (weight >=5) 
    	{
-   		totalAmount = weight * 8.00;
+   		cost = weight * 8.00;
    		System.out.print("For a parcel weight of " + weight);
-   	 	System.out.println("kg, the cost is RM" + totalAmount);
+   	 	System.out.println("kg, the cost is RM" + cost);
    	}
    	else if (weight == 0) 
    	{
-   		totalAmount = totalAmount * 0;
+   		cost = cost * 0;
    
    	}
-	return totalAmount;
+	return cost;
 }
 public static double computeInternationalCost (double weight) {
 	 grams = weight * 1000;
 	 if (grams <=250)
-	 {totalAmount = 41.20;
+	 {cost = 41.20;
 		 System.out.print("For a parcel weight of " + weight);
-       	 System.out.println("kg, the cost is RM" + totalAmount);
+       	 System.out.println("kg, the cost is RM" + cost);
 	 }
 	 else if (grams > 250 && weight <= 20)
 	 {
-		 totalAmount =(Math.ceil((grams - 250)/250)*5.20) + 41.20;
+		 cost =(Math.ceil((grams - 250)/250)*5.20) + 41.20;
 		 System.out.print("For a parcel weight of " + weight);
-       	 System.out.println("kg, the cost is RM" + totalAmount);
+       	 System.out.println("kg, the cost is RM" + cost);
 	 }
 	 else if (grams == 0)
 	 {numOfIParcels--;
-		 totalAmount = 0;
+		 cost = 0;
 	 }
-	 totalAmount = totalAmount*1;
-	 return totalAmount;
+	 return cost;
  }
 public static void displayFinalOutput (int numOfIParcels, int numOfDParcels, int rejectedParcels, double totalAmount) {
+	if (numOfDParcels == 0 && numOfIParcels == 0 && i == 0)
+	{
+		System.out.print("Number of rejected parcels is " + rejectedParcels);
+		System.out.print("\n No parcels to send today!");
+	}
+	else{
 System.out.println("Number of rejected parcels is " + rejectedParcels);
 System.out.println("Number of Domestic parcels is " + numOfDParcels);
 System.out.println("Number of International parcels is " + numOfIParcels);
 System.out.println("Number of total parcels is" + (numOfDParcels+numOfIParcels));
 System.out.println("With total amount of " + totalAmount);
+}
 }
 }

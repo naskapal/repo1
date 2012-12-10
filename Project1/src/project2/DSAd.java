1d9b91586e93c3bce207c5cf2a73e651df15bd74
naskapal <co.terbang@gmail.com>
2012-12-10 21:11:24 +0800

    unfinished, but, better than before

--- Project1/src/project2/DSAd.java
+++ Project1/src/project2/DSAd.java
@@ -10,15 +10,23 @@ public static void main (String [] args){
 	parcel parcel0 = new parcel (2,'d');
 	parcel parcel1 = new parcel (3,'i');
 	parcel parcel2 = new parcel (1,'d');
+	parcel parcel3 = new parcel (3,'i');
 	parcelList asd = new parcelList();
 	//parcelList dsa = new parcelList(2);
 	asd.addParcel(parcel0);
 	asd.addParcel(parcel1);
 	asd.addParcel(parcel2);
+	asd.addParcel(parcel3);
 	//asd.allParcels();
-	asd.test();
 	//dsa.test();
 	//System.out.println(asd.getNumOfParcels());
-	System.out.print(asd.totalCost());
+	tcost = asd.totalCost();
+	//System.out.print("\n" + tcost);
+	//for (int i = 0;i<9;i++)
+	//{
+	//System.out.println(asd.test()[i]);
+	//}
+	//System.out.print("\n" + asd.getMaxCostParcel());
+	asd.getParcelByCost(1.0, 98.4);
 }
 }
--- Project1/src/project2/parcelList.java
+++ Project1/src/project2/parcelList.java
@@ -24,9 +24,7 @@ public boolean addParcel (parcel x) {
 	if (numOfParcels != newParcel.length)
 	{
 		newParcel[numOfParcels] = x;
-		//System.out.println(numOfParcels);
 		numOfParcels++;
-		//System.out.println(numOfParcels);
 		return true;
 	}
 	else
@@ -46,94 +44,98 @@ public String allParcels() {
 	}
 	return msg;
 }
-public void test() {
+public String[] test() {
+	String[] parcels = new String [newParcel.length];
 	for (int i=0;i<newParcel.length;i++)
 	{
 		if (newParcel[i] == null)
 		{
-			System.out.println("null");
+			parcels[i] = "null";
 		}
 		else
 		{
-			System.out.println(i + "." + newParcel[i].toString());
+			parcels[i] = i + "." + newParcel[i].toString();
 		}
 	}
-	System.out.println(newParcel[1].getCost());
+	return parcels;
 }
 public double totalCost() {
 	double tcost = 0;
 	for (int i = 0;i<newParcel.length;i++)
 	{
-		tcost = tcost + newParcel[i].getCost();
+		if (newParcel[i] == null)
+		{
+			tcost = tcost + 0;
+		}
+		else
+		{
+			tcost = tcost + newParcel[i].getCost();
+		}
 	}
 	return tcost;
 }
-public double getMaxCostParcel() {
+public String getMaxCostParcel() {
 	double maxCost = newParcel[0].getCost();
+	String msg = "";
 	for (int i=1;i<newParcel.length;i++)
 	{
-		if(newParcel[i].getCost() > maxCost)
+		if (newParcel[i] != null)
 		{
-			maxCost = newParcel[i].getCost();
-		}
+			if(newParcel[i].getCost() > maxCost)
+			{
+				maxCost = newParcel[i].getCost();
+			}	
+		}msg = "Found the max cost parcel at index " + i + " with price " + maxCost; // not 100% correct, need more think
 	}
-	return maxCost;
+	return msg;
 }
-public double getMinCostParcel() {
+public String getMinCostParcel() {
 	double minCost = newParcel[0].getCost();
+	String msg = "";
 	for (int i=1;i<newParcel.length;i++)
 	{
+		if (newParcel[i] != null)
+		{
 		if(newParcel[i].getCost() < minCost)
 		{
 			minCost = newParcel[i].getCost();
 		}
+		}msg = "Found the min cost parcel at index " + i + " with price " + minCost; //index numbering is still incorrect, like in maxcost, need more think
 	}
-	return minCost;
+	return msg;
 }
 public void getParcelByCost(double cost1, double cost2) {
-	boolean found1= false, found2 = false;
+	double minCost = cost1, maxCost = cost2;
+	boolean check = false;
 	int i = 0;
-	while (i < newParcel.length)
+	for (i = 0;i<newParcel.length;i++)
 	{
-		if (cost1 == newParcel[i].getCost())
+		if (newParcel[i] != null)
 		{
-			found1 = true;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	while (i < newParcel.length)
-	{
-		if (cost2 == newParcel[i].getCost())
-		{
-			found2 = true;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	if (found1 == true && found2 == true)
-	{
-		System.out.print(cost1);
-		for (i=0; i < newParcel.length;)
-		{
-			if (newParcel[i].getCost() >= cost1 && newParcel[i].getCost() <= cost2)
+			if (minCost == newParcel[i].getCost())
 			{
-				System.out.print("\n" + newParcel[i]);
-			}
-			else
-			{
-				i++;
+				if (maxCost == newParcel[i].getCost())
+				{
+					check = true;
+				}
 			}
 		}
-		System.out.print("\n" + cost2);
-	}
-	else
-	{
-		System.out.print("");
+		if (check == true)
+		{
+			for (i = 0;i<newParcel.length;i++)
+			{
+				if (newParcel[i] != null)
+				{
+					if (newParcel[i].getCost() >= minCost)
+					{
+						if (newParcel[i].getCost() <= maxCost)
+						{
+							System.out.print("\n" + newParcel[i].getCost());
+						}
+					}
+				}
+			}
+		}
 	}
 }
 public void getParcelsByDestination (char a) {
